e2c8dc692c6f482a82c960d3a4914077319553d1
Nerd_Destroyer <s1098659@student.knoxschools.org>
2026-01-28 14:17:52 -0500

    Create Compute_Data.java

---
+++ calculator/Compute_Data.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Compute_Data{
+  public static void main(String[] args){
+    Scanner ui = new Scanner(System.in);
+    System.out.println("This program is made only to compute a list of variables.");
+    System.out.print("First type your list of variables (separate with spaces or commas): ");
+    int List = ui.nextInt();
+    int Variable_List[] = {List}; 
+    System.out.println("Type the full definition or the acronym of what's changing your list. (Ex: Square root your y variables. So type sqrt or Square Root)");
+    
+    String Operation = ui.nextLine();
+
+    
+
+    
+  }
+}
