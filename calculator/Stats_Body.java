207f02fd987380f042b3642542db6cebad2296a5
Nerd_Destroyer <s1098659@student.knoxschools.org>
2026-01-29 21:14:07 -0500

    Rename Compute_Data to Stats_Calc_Body

---
+++ calculator/Stats_Body.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Stats_Calc_Body{
+  public static void main(String[] args){
+    Scanner ui = new Scanner(System.in);
+    System.out.print("Type AP Stats unit: ");
+    String Unit_Choice = ui.nextLine();
+
+    
+  
+    
+  }
+}
